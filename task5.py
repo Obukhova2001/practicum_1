3f0aa471bd87fc817e9e570f798ddcfa8d7e6a2e
Obukhova2001 <73060926+Obukhova2001@users.noreply.github.com>
2020-12-04 09:07:07 +0500

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+Имя проекта: Boring-numpy
+Номер версии: 1.0
+Имя файла: practicum-1(1-101).py
+Автор: 2019 © Д.А.Обухова, Челябинск
+Лицензия использования:  CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/deed.ru)
+Дата создания: 11/11/2020
+Дата последней модификации: 11/11/2020
+Связанные файлы/пакеты: numpy, random
+Описание: Решение задач № 1-101 практикума № 1
+#версия Python: 3.8.5
+"""
+b = 256
+klb = float (b) / float (1024)
+mgb = float (klb) / float (1024)
+print (klb)
