734a40caf64b7363512c5ce97b73dcab25530b35
Obukhova2001 <73060926+Obukhova2001@users.noreply.github.com>
2020-12-25 00:30:13 +0500

    Add files via upload

---
+++ task65.py
@@ -0,0 +1,37 @@
+"""
+Имя проекта: Boring-numpy
+Номер версии: 1.0
+Имя файла: practicum-1(1-101).py
+Автор: 2019 © Д.А.Обухова, Челябинск
+Лицензия использования:  CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/deed.ru)
+Дата создания: 11/11/2020
+Дата последней модификации: 11/11/2020
+Связанные файлы/пакеты: numpy, random
+Описание: Решение задач № 1-101 практикума №65
+Вводятся положительные числа. Определить сумму чисел, делящихся на положительное число B нацело. При вводе отрицательного числа закончить работу.
+#версия Python: 3.6
+"""
+import re
+
+list_numbers = []
+
+B = 5
+sum = 0
+
+while True:
+    print("Введите число:", end=' ')
+    string = re.sub(r'[^0-9\-]+', '', input())
+    if len(string) == 0:
+        print("В строке не обнаружено числа")
+        continue
+
+    number = int(string)
+    list_numbers.append(number)
+
+    if number < 0:
+        break
+
+    if number % B == 0:
+        sum += number
+
+print("Сумма:", sum)
