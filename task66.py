c88085da987afd76b635f93a52ca210bf0c9c6ff
Obukhova2001 <73060926+Obukhova2001@users.noreply.github.com>
2020-12-25 00:32:23 +0500

    Add files via upload

---
+++ task66.py
@@ -0,0 +1,43 @@
+"""
+Имя проекта: Boring-numpy
+Номер версии: 1.0
+Имя файла: practicum-1(1-101).py
+Автор: 2019 © Д.А.Обухова, Челябинск
+Лицензия использования:  CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/deed.ru)
+Дата создания: 11/11/2020
+Дата последней модификации: 11/11/2020
+Связанные файлы/пакеты: numpy, random
+Описание: Решение задач № 1-101 практикума №66
+Для вводимых чисел определить процент положительных и отрицательных чисел. При вводе числа −65432 закончить работу.
+#версия Python: 3.6
+"""
+import re
+
+list_numbers = []
+
+count_positive = 0
+count_negative = 0
+
+while True:
+    print("Введите число:", end=' ')
+    string = re.sub(r'[^0-9\-]+', '', input())
+    if len(string) == 0:
+        print("В строке не обнаружено числа")
+        continue
+
+    number = int(string)
+
+    if number * -1 == 65432:
+        break
+
+    list_numbers.append(number)
+
+    if number < 0:
+        count_negative += 1
+    else:
+        count_positive += 1
+
+
+one_percent = 100 / len(list_numbers)
+print("Процент положительных чисел:", one_percent * count_positive)
+print("Процент отрицательных чисел:", one_percent * count_negative)
